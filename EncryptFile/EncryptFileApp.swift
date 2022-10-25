fda9b0168d2cd11a6203fc206aacea0a00c97288
Сафир Михаил Дмитриевич [B] <mdsafir@sberbank.ru>
2022-10-25 22:04:08 +0300

    Initial Commit

---
+++ EncryptFile/EncryptFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncryptFileApp.swift
+//  EncryptFile
+//
+//  Created by Michael Safir on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct EncryptFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
